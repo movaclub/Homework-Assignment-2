5c56332b0169b73667c2cf69653e8b705921401d
movaclub <seeusers@gmail.com>
2018-10-21 11:10:18 +0300

    wip: routing start

--- handlers/index.js
+++ handlers/index.js
@@ -1,6 +1,7 @@
-// handlers
+// handlers & helpers
 
 // prerequisites
+// const crypto = require('crypto');
 
 // user-defined libs
 const menu = require('./../menu'); // index.js - module
@@ -8,6 +9,7 @@ const users = require('./../users'); // index.js - module, users.json - user DB
 
 // reusable user object
 const user = {
+  id       :null,
   full_name:'',
   str_addr: '',
   password: '',
@@ -19,14 +21,23 @@ const handlers = {};
 // create a new user acc: data - user object: { /- see above -/ }
 handlers.usrAdd = (data, cb) => {
 
-  user.full_name = data.full_name;
-  user.str_addr  = data.str_addr;
-  user.password  = data.password;
-  user.email     = data.email;
+  if (typeof(data) !== 'undefined'){ // a subtle trait of checking
 
-  users.create(user, (err) => {
+    user.full_name = data.full_name;
+    user.str_addr  = data.str_addr;
+    user.password  = data.password;
+    user.email     = data.email;
+    cb({status:200,saved:true});
 
-  });
+
+  } else {
+    cb({status:200,saved:false});
+  }
+  console.log('handlers.usrAdd: ', user);
+
+//   users.create(user, (err) => {
+//
+//   });
 };
 
 // upd a user record
@@ -39,6 +50,29 @@ handlers.usrUpd = (data, cb) => {
 handlers.usrDel = (data, cb) => {
   user = data;
 
+};
+
+
+// Not-Found method status
+handlers.notFound = (data,callback) => {
+  callback(404);
+};
+
+
+// Create a session id (SID)
+handlers.createRandomString = () => {
+
+  // generator alphabet
+  const possibleCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_';
+  let str = '';
+  for(i = 1; i <= 13; i++) {
+      // Get a random charactert from the possibleCharacters string
+      let randomCharacter = possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+      // Append this character to the string
+      str+=randomCharacter;
+  }
+  // Return the final string
+  return str;
 };
 
 // TEST: menu output
--- index.js
+++ index.js
@@ -7,10 +7,13 @@ const StringDecoder = require('string_decoder').StringDecoder;
 // handers - app useful methods
 const handlers = require('./handlers'); // index.js - module
 
-// console.log("MENU: ", menu);
+// the server acceptable HTTP methods
+const acceptableMethods = ['post','get','put','delete'];
 
-// handlers
-// handlers.menu();
+// API routes
+const routes = {
+  'user/add': handlers.usrAdd
+};
 
 let server = http.createServer((req,res) => {
 
@@ -24,13 +27,61 @@ let server = http.createServer((req,res) => {
   // get the HTTP method
   let method = req.method.toLowerCase();
 
+  // get the headers
+  let headers = req.headers;
+
+  // get the payload, if any
+  let decoder = new StringDecoder('utf-8');
+  let buffer = '';
+
+  req.on('data', (data) => {
+    buffer += decoder.write(data);
+  });
+
+  // spit out the response
+  req.on('end', () => {
+
+    buffer += decoder.end();
+
+    const datum = {
+      'trimmedPath': trimmedPath,
+      'method'     : method,
+      'headers'    : headers,
+      'payload'    : JSON.parse(buffer)
+    };
+
+    // console.log('DATUM: ', datum);
+    res.setHeader('Content-Type', 'application/json');
+
+    // method check
+    if ( acceptableMethods.indexOf(method) > -1 ){
+
+      // process the request
+      if ( typeof(routes[trimmedPath]) !== 'undefined' ){
+        let chosenHandler = routes[trimmedPath];
+        chosenHandler(datum.payload, (ret) => {
+          // console.log('RET: ', ret);
+          res.writeHead(ret.status);
+          res.end(JSON.stringify(ret));
+        });
+
+      } else {
+          res.writeHead(404); // not found
+          res.end();
+      }
+
+    } else {
+          res.writeHead(405); // not allowed
+          res.end();
+    }
+
+  });
 
 
-  // get headers
-
-  // res.end('server is up & running...');
 });
 
 server.listen(3000, () => {
   console.log('server is up & running on port 3000');
 });
+
+// TypeError [ERR_INVALID_ARG_TYPE]: The "chunk" argument must be one of type string or Buffer. Received type object
